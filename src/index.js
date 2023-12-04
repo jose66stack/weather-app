83de5828db25ebad46ef4a1ecf9b8de39409c7b5
jose66stack <jfp127631@gmail.com>
2023-12-04 12:32:25 -0600

    v1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// ./src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from './reportWebVitals'; // Remove or comment out this line
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
